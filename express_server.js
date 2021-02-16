e711bdb76602fa438ada90a3be0d376003f84fbf
Angel Sinn <angelkysinn@gmail.com>
2021-02-16 00:51:29 +0000

    Added POST route to receive form submission

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -12,6 +14,11 @@ const urlDatabase = {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/new", (req, res) => {
